6177347926044e28c816fc276a51ded1e4751758
Peter Fritchley <peter@propcom.co.uk>
2013-01-31 09:59:27 +0000

    use newer versions of javascript files

--- js/category.js
+++ js/category.js
@@ -4,65 +4,60 @@ $(function(){
     function on_filter_change() {
         // Build filter string
         var $this = $(this);
-        var filterStr = '';
-
-       $('.filters :input.filter').each(function(i, elm){
-
-           if ($(elm).is(':checkbox') && ! $(elm).is(':checked'))
-               return;
-
-           var filterName = $(elm).attr('name');
-           var filterVal = $(elm).val();
-           filterStr += filterName + '[]=' + filterVal + '&';
-       });
-
-        console.log(filterStr);
-        // Ajax
-        var url = '/search/index/?' + filterStr + window.location.search.slice(1);
-
-        window.history.pushState("", "", url);
-
-        filter_request = $.get(url, function(response) {
-            $('#main-content').html(response);
-
-            //init_slider();
-        });
-    }
-
-    $('.filters :input.filter').live('click', function() {
-        var $this = $(this);
+        var $form = $this.closest('form');
 
         // Abort the active request
         if (filter_request) {
             filter_request.abort();
         }
 
-        /* if (! $this.hasClass('show-all')) {
-            $this.toggleClass('checked');
-
-            // Not the same as .siblings() - .siblings() doesn't include $this.
-            if (! $this.parent().children('a.checked').length) {
-                $this.closest('.filter').find('a.show-all').addClass('checked');
+        filter_request = $.get('/search/index', $form.serialize(), function(response) {
+            $('#search-content').replaceWith(response);
+            filter_request = null;
+            var state = $.param($form.serializeArray());
+            if (state.length > 0) {
+                state = '?' + state;
             }
-            else {
-                $this.closest('.filter').find('a.show-all').removeClass('checked');
-            }
-        }
-        else {
-            if (!$this.hasClass('checked')) {
-                $this.toggleClass('checked');
-                $this.parent().find('nav a').removeClass('checked');
-            }
-            else {
-                return false;
-            }
-        }
-         */
-        $this.trigger('filter-change');
+            history.pushState(state, "", "/search/index"+state);
+            setupFilters();
+            setupSort();
+        });
+    }
 
-        return false;
-    })
-    .on('filter-change', on_filter_change);
+    function setupFilters() {
+        $('.js-search-filter').on('click', function(e) {
+            e.preventDefault();
+            var $this = $(this);
 
+            // Abort the active request
+            if (filter_request) {
+                filter_request.abort();
+            }
 
+            if ($this.is('a')) {
+                var $input = $this.prev();
+
+                if ($input.is(':checked')) {
+                    $input.attr('checked', false);
+                }
+                else {
+                    $input.attr('checked', true);
+                }
+            }
+
+            $this.trigger('filter-change');
+
+            return false;
+        })
+        .on('filter-change', on_filter_change);
+    }
+
+    function setupSort() {
+        $('.js-search-sort').on('change', function(e){
+           $(this).trigger('filter-change');
+        }).on('filter-change', on_filter_change);
+    }
+
+    setupFilters();
+    setupSort();
 });
--- js/minibasket.js
+++ js/minibasket.js
@@ -1,6 +1,6 @@
 (function(){
 	$(document).ready(function(){
-		$('body').delegate('ul.mini-basket-dropdown').on('basketChanged.basket.data-api', function(e){
+		$('body').delegate('.mini-basket-dropdown').on('basketChanged.basket.data-api', function(e){
 			$.get('/basket/mini', function(data){
 				$('.mini-basket-wrap').replaceWith(data);
 			});
