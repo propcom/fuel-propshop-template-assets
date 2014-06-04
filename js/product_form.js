afd88b51f3e5a08af3fb4cf80b592750063f1cab
Robert O'Brien <robert.obrien@propcom.co.uk>
2014-06-04 15:17:18 +0100

    [product_form.js] Removed double exclamation mark

--- js/product_form.js
+++ js/product_form.js
@@ -222,7 +222,7 @@
 
         filled_fields: function() {
             return this.fields.filter(function() {
-                return !!$.trim($(this).val());
+                return !$.trim($(this).val());
             });
         },
 
@@ -239,7 +239,7 @@
             if (unfilled.length)
             {
                 e.preventDefault();
-                
+
                 var fields = [];
 
                 unfilled.each(function(i,o) {
