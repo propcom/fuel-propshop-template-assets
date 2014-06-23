e85cf736bae22694a766635cb3405e41c1156927
Phil Lavin <phil@propcom.co.uk>
2014-06-23 20:40:52 +0100

    Fix up the checkout address JS so as it's not always broken

--- js/address-form.js
+++ js/address-form.js
@@ -1,175 +1,184 @@
-(function($){
-    function AddressForm(form, prefix) {
-        var self = this;
-        this.$form = $(form);
-        this.prefix = prefix;
+(function ($) {
+	function AddressForm(form, prefix) {
+		var self = this;
+		this.$form = $(form);
+		this.prefix = prefix;
 
-        this.$form.find('.js-country-code').on('change', function() {
-            self.update_states($(this).val());
-        });
+		this.$form.find('.js-country-code').on('change', function () {
+			self.update_states($(this).val());
+		});
 
-        this.$form.find('.js-address-select').on('change', function() {
-            var $selected = $(this).find('option:selected');
-            var data;
-            if (data = $selected.data('address')) {
-                self.set_values(data);
-                return;
-            }
+		this.$form.find('.js-address-select').on('change', function () {
+			var $selected = $(this).find('option:selected');
+			var data;
+			if (data = $selected.data('address')) {
+				self.set_values(data);
+				return;
+			}
 
-            $.getJSON('/customer/rest/address.json', {
-                    id: $(this).val()
-                })
-                .success(function(data){
-                    $selected.data('address', data);
-                    self.set_values(data);
-                });
-        });
+			$.ajax('/customer/rest/address.json', {
+				'type': 'get',
+				'data': {
+					id: $(this).val()
+				},
+				'dataType': 'json',
+				'success': function (data) {
+					$selected.data('address', data);
+					self.set_values(data);
+				}
+			});
+		});
 
-        this.$form.data('address-form', self);
-    }
+		this.$form.data('address-form', self);
+	}
 
-    AddressForm.prototype = {
-        constructor: AddressForm,
-        update_states: function(country, andthen) {
-            var self = this;
+	AddressForm.prototype = {
+		constructor: AddressForm,
+		update_states: function (country, andthen) {
+			var self = this;
 
-            $.get('/customer/rest/address_states.json', {
-                country_code: country
-            }, function(data) {
-                var $state_select = self.$form.find('.js-state-code').empty();
+			$.ajax('/customer/rest/address_states.json', {
+				'type': 'get',
+				'async': false,
+				'data': {
+					country_code: country
+				},
+				'dataType': 'json',
+				'success': function (data) {
+					var $state_select = self.$form.find('.js-state-code');
 
-                $.each(data, function(code, name) {
-                    var $opt = $('<option/>');
+					$state_select.empty();
 
-                    $opt.attr('value', code);
-                    $opt.html(name);
+					$.each(data, function (code, name) {
+						var $opt = $('<option/>');
 
-                    $state_select.append($opt);
-                });
-                
-                $state_select.trigger('change.address-form');
+						$opt.attr('value', code);
+						$opt.html(name);
 
-                if (andthen) andthen($state_select);
-            })
-        },
-        set_values: function(values) {
-            var self = this;
+						$state_select.append($opt);
+					});
 
-            // Avoid changing the thing we passed in by reference
-            values = $.extend({}, values);
+					$state_select.trigger('change.address-form');
 
-            if (values.country_code) {
-                var state_code = values.state_code;
+					if (andthen) andthen($state_select);
+				}
+			});
+		},
+		set_values: function (values) {
+			var self = this;
 
-                self.$form.find('.js-country-code').val(values.country_code).trigger('change.address-form');
+			// Avoid changing the thing we passed in by reference
+			values = $.extend({}, values);
 
-                this.update_states(values.country_code, function($state_select) {
-                    if (state_code) {
-                        $state_select.val(state_code);
-                        $state_select.trigger('change.address-form');
-                    }
-                });
-            }
-            else if (values.state_code) {
-                self.$form.find('.js-state-code').val(values.state_code).trigger('change.address-form');
-            }
+			if (values.country_code) {
+				var state_code = values.state_code;
 
-            delete values['country_code'];
-            delete values['state_code'];
+				self.$form.find('.js-country-code').val(values.country_code).trigger('change.address-form');
 
-            $.each(values, function(field,val) {
-                var input = self.$form.find('[name="' +  self.prefix + field + '"]')
-                input.val(val);
-                input.trigger('change.address-form');
-            });
-        },
-        get_values: function() {
-            var self = this;
-            var values = {};
+				self.update_states(values.country_code, function ($state_select) {
+					if (state_code) {
+						$state_select.val(state_code);
+						$state_select.trigger('change.address-form');
+					}
+				});
+			} else if (values.state_code) {
+				self.$form.find('.js-state-code').val(values.state_code).trigger('change.address-form');
+			}
 
-            self.$form.find(':input').each(function(_, input) {
-                var $this = $(this);
-                values[$this.attr('name').replace(self.prefix, '')] = $this.val();
-            });
+			delete values['country_code'];
+			delete values['state_code'];
 
-            return values;
-        },
-        couple: function (other_form) {
-            if (this._couple)
-                return;
+			$.each(values, function (field, val) {
+				var input = self.$form.find('[name="' + self.prefix + field + '"]')
+				input.val(val);
+				input.trigger('change.address-form');
+			});
+		},
+		get_values: function () {
+			var self = this;
+			var values = {};
 
-            this._couple = other_form;
+			self.$form.find(':input').each(function (_, input) {
+				var $this = $(this);
+				values[$this.attr('name').replace(self.prefix, '')] = $this.val();
+			});
 
-            this._couple.$form.find(':input').on('change.address-form', this._on_couple_change());
-            this.set_values(other_form.get_values());
-        },
-        _on_couple_change: function() {
-            var self = this;
+			return values;
+		},
+		couple: function (other_form) {
+			if (this._couple)
+				return;
 
-            this._couple_handler = this._couple_handler || function(event) {
-                // If we're changing country_code, then the update handler on state_code will trigger its own change
-                // event, so that'll be magically handled when the AJAX completes.
-                var name = $(this).attr('name').replace(self._couple.prefix, ''),
-                    val = $(this).val();
+			this._couple = other_form;
 
-                var values = {};
-                values[name] = val;
-                self.set_values(values);
-            }; 
+			this._couple.$form.find(':input').on('change.address-form', this._on_couple_change());
+			this.set_values(other_form.get_values());
+		},
+		_on_couple_change: function () {
+			var self = this;
 
-            return this._couple_handler;
-        },
-        decouple: function() {
-            if (this._couple) {
-                this._couple.$form.find(':input').off('change.address-form');
-                this._couple = null;
-            }
-        }
-    };
+			this._couple_handler = this._couple_handler || function (event) {
+				// If we're changing country_code, then the update handler on state_code will trigger its own change
+				// event, so that'll be magically handled when the AJAX completes.
+				var name = $(this).attr('name').replace(self._couple.prefix, ''),
+					val = $(this).val();
 
-    window.AddressForm = AddressForm;
+				var values = {};
+				values[name] = val;
+				self.set_values(values);
+			};
 
-    var addressForms = [];
+			return this._couple_handler;
+		},
+		decouple: function () {
+			if (this._couple) {
+				this._couple.$form.find(':input').off('change.address-form');
+				this._couple = null;
+			}
+		}
+	};
 
-    window.AddressForms = {
-        register: function($form, prefix) {
-            if (! ($form instanceof AddressForm)) {
-                $form.each(function(){
-                    addressForms.push(new AddressForm($(this), prefix));
-                });
-            }
-            else {
-                addressForms.push($form, prefix);
-            }
-        }
-    };
+	window.AddressForm = AddressForm;
+
+	var addressForms = [];
+
+	window.AddressForms = {
+		register: function ($form, prefix) {
+			if (!($form instanceof AddressForm)) {
+				$form.each(function () {
+					addressForms.push(new AddressForm($(this), prefix));
+				});
+			} else {
+				addressForms.push($form, prefix);
+			}
+		}
+	};
 })(jQuery);
 
-$(function() {
-    var $billing_form = $('.js-billing-form'),
-        $delivery_form = $('.js-delivery-form');
+$(function () {
+	var $billing_form = $('.js-billing-form'),
+		$delivery_form = $('.js-delivery-form');
 
-    AddressForms.register($billing_form, 'billing_');
-    AddressForms.register($delivery_form, 'delivery_');
+	AddressForms.register($billing_form, 'billing_');
+	AddressForms.register($delivery_form, 'delivery_');
 
-    $('.js-copy-address').on('change', function() {
-        var $this = $(this);
-        var from = $this.data('from');
+	$('.js-copy-address').on('change', function () {
+		var $this = $(this);
+		var from = $this.data('from');
 
-        var on = !!$this.is(':checked');
+		var on = !!$this.is(':checked');
 
-        if (from == 'billing') {
-            if (on)
-                $delivery_form.data('address-form').couple($billing_form.data('address-form'));
-            else
-                $delivery_form.data('address-form').decouple();
-        }
-        else {
-            if (on)
-                $billing_form.data('address-form').couple($delivery_form.data('address-form'));
-            else
-                $billing_form.data('address-form').decouple();
-        }
-    }).trigger('change');
+		if (from == 'billing') {
+			if (on)
+				$delivery_form.data('address-form').couple($billing_form.data('address-form'));
+			else
+				$delivery_form.data('address-form').decouple();
+		} else {
+			if (on)
+				$billing_form.data('address-form').couple($delivery_form.data('address-form'));
+			else
+				$billing_form.data('address-form').decouple();
+		}
+	}).trigger('change');
 
 });
