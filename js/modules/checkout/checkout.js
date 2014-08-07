c8bd2a059d4d80481287b5345d1e5ba4b405c6d4
cesar <cesar.castro@propcom.co.uk>
2014-08-07 17:01:11 +0100

    checkout login

--- js/modules/checkout/checkout.js
+++ js/modules/checkout/checkout.js
@@ -1,13 +1,54 @@
-$(document).ready(function () {
-    if ( $('#shipping_chk').is(':checked')) {
-        $('#js-shipping-form').hide();
-    }
+// $(document).ready(function () {
+//     if ( $('#shipping_chk').is(':checked')) {
+//         $('#js-shipping-form').hide();
+//     }
 
-    $('#shipping_chk').on('click', function () {
-        if ( ! $(this).is(':checked')) {
-            $('#js-shipping-form').show();
-        } else {
-            $('#js-shipping-form').hide();
+//     $('#shipping_chk').on('click', function () {
+//         if ( ! $(this).is(':checked')) {
+//             $('#js-shipping-form').show();
+//         } else {
+//             $('#js-shipping-form').hide();
+//         }
+//     });
+// });
+
+
+document.getElementById('propshop-checkout')&& (function($){
+
+    var signupForm = $('#js-ps-checkout-signup-form'), loginForm = $('#js-ps-checkout-login-form');
+
+    loginForm.css('height', function(){
+         return loginForm.children('div').get(0).clientHeight + 'px';
+    });
+
+    $(document).on('click', '#js-ps-checkout-signup-btn', function(e){
+
+        e.preventDefault();
+
+        if(!signupForm.hasClass('is-open')){
+
+            signupForm.css('height', function(){
+                return signupForm.children('div').get(0).clientHeight + 'px';
+            }).addClass('is-open');
+
+            loginForm.css('height', '0px').removeClass('is-open');
         }
     });
-});
+
+
+    $(document).on('click', '#js-ps-checkout-login-btn', function(e){
+
+        e.preventDefault();
+
+        if(!loginForm.hasClass('is-open')){
+
+            loginForm.css('height', function(){
+                return loginForm.children('div').get(0).clientHeight + 'px';
+            }).addClass('is-open');
+
+            signupForm.css('height', '0px').removeClass('is-open');
+        }
+    });
+
+
+}(jQuery));
