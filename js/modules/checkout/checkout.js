028e741eb57c4a12059843fd1b62df21923e44ea
cesar <cesar.castro@propcom.co.uk>
2014-08-13 12:12:06 +0100

    payment form card validator

--- js/modules/checkout/checkout.js
+++ js/modules/checkout/checkout.js
@@ -68,3 +68,34 @@ document.getElementById('propshop-checkout')&& (function($){
 }(jQuery));
 
 
+document.getElementById('sage-payment-form') && (function($){
+
+    var number = $('#payment_details--card_number'), type = $('#payment_details--card_type'), template = type.attr('data-template');
+
+   number.validateCreditCard(function(result){
+
+        var matches = $.parseJSON(type.attr('data-matches'));
+
+        console.log(matches);
+
+         console.log(result);
+
+        if(result.length_valid && result.luhn_valid){
+
+            type.val(matches[result.card_type.name]);
+
+           number.parent().css('position', 'relative').append('<span class="input__card-image" id="card-helper"><img src="'+template+result.card_type.name+'.svg" onerror="this.onerror=null; this.src=\''+template+result.card_type.name+'.png\'" /></span>')
+
+
+
+        } else if(!result.length_valid || result.luhn_valid){
+
+            number.parent().find('#card-helper').remove();
+            //trigger validation
+        }
+
+    }, { accept: $.parseJSON(type.attr('data-valid')) });
+
+}(jQuery))
+
+
