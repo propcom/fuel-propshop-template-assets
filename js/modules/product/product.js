ff1185011a1399b9d0e22b65ad600912214e5630
Phil Lavin <phil@propcom.co.uk>
2014-08-22 16:04:14 +0100

    Implemented variant loading on change of any field in the product form

--- js/modules/product/product.js
+++ js/modules/product/product.js
@@ -1,158 +1,155 @@
 //Form: this sets up the variant selects.
-
-
-document.getElementById('js-prop-add-to-basket-form') && (function($){
+document.getElementById('js-prop-add-to-basket-form') && (function($) {
 
 	"use strict";
 
+	var onVariantChange = function(e, data) {
+		loadVariant(data.selected_variant_id, before, after);
+	};
 
-	var loadVariant = function(variant, beforeStart, callback){
+	var loadVariant = function(variant, beforeStart, callback) {
 
-		if(typeof beforeStart === 'function')
+		if (typeof beforeStart === 'function')
 			beforeStart(document.getElementById('js-ps-product-info-container'));
 
+		/**
+		 * Using jQuery.get() to request the new variant asynchronously
+		 *
+		 * http://api.jquery.com/jquery.get/
+		 */
+
+		$.get('/product/view_info/' + variant).done(function(data) {
+
 			/**
-			 * Using jQuery.get() to request the new variant asynchronously
+			 * Replacing the markup in the variant's info container by the response returned
 			 *
-			 * http://api.jquery.com/jquery.get/
+			 * http://api.jquery.com/replaceWith/
 			 */
+			$('#js-ps-product-info-container').replaceWith(data);
 
-			 $.get('/product/view_info/'+variant).done(function(data){
-
-				/**
-				 * Replacing the markup in the variant's info container by the response returned
-				 *
-				 * http://api.jquery.com/replaceWith/
-				 */
-				 $('#js-ps-product-info-container').replaceWith(data);
-
-				/**
-				 * Updating the history state
-				 *
-				 * https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Manipulating_the_browser_history
-				 */
-				 if(history.pushState)
-				 {
-				 	history.pushState({variant: variant }, "", '/product/view/'+variant );
-				 }
-
-				/**
-				 * Registering the form included in the response
-				 */
-				 ProductForms.register($('#js-prop-add-to-basket-form'));
-
-				/**
-				 * Wrapping the select elements included in the response
-				 */
-				 wrapSelects();
-
-				 if(typeof callback === 'function')
-				 	callback(document.getElementById('js-ps-product-info-container'));
-
-				});
-
+			/**
+			 * Updating the history state
+			 *
+			 * https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Manipulating_the_browser_history
+			 */
+			if (history.pushState) {
+				history.pushState({
+					variant: variant
+				}, "", '/product/view/' + variant);
 			}
 
+			/**
+			 * Registering the form included in the response
+			 */
 			var prod_form = $('#js-prop-add-to-basket-form');
 
-			ProductForms.register(prod_form, function(){
+			ProductForms.register(prod_form);
 
-				$(document).on('set_option', '#js-prop-add-to-basket-form .variant-select', function (e, data) {
+			prod_form.on('variant_changed', onVariantChange);
 
-					var variant_id = data.product_form.selected_variant.val();
+			/**
+			 * Wrapping the select elements included in the response
+			 */
+			wrapSelects();
 
-					loadVariant(variant_id, before, after)
+			if (typeof callback === 'function')
+				callback(document.getElementById('js-ps-product-info-container'));
 
-				});
+		});
 
-			});
+	}
 
+	var prod_form = $('#js-prop-add-to-basket-form');
 
+	ProductForms.register(prod_form);
+
+	prod_form.on('variant_changed', onVariantChange);
+
+	/**
+	 * Click event handler for product Colours
+	 *
+	 * Loads in asynchronously the new colour variant
+	 * Replaces the whole variant info container with the loaded data
+	 * Registers the loaded form
+	 * Styles the loaded select elements
+	 */
+	$('body').on('click', '.js-ps-product-color', function(e) {
+		/**
+		 * Preventing the links default behaviour
+		 */
+		e.preventDefault();
 
 		/**
-		 * Click event handler for product Colours
-		 *
-		 * Loads in asynchronously the new colour variant
-		 * Replaces the whole variant info container with the loaded data
-		 * Registers the loaded form 
-		 * Styles the loaded select elements
+		 * caching the jQuery object corresponding to the element
 		 */
-		 $('body').on('click', '.js-ps-product-color', function(e){
-			/**
-			 * Preventing the links default behaviour
-			 */
-			 e.preventDefault();
+		var self = $(this);
 
-			/**
-			 * caching the jQuery object corresponding to the element
-			 */
-			 var self = $(this);
+		loadVariant(self.attr('data-variant'), before, after);
+
+	});
+
+	var before = function(elem) {
+		if (elem.style.opacity)
+			elem.style.opacity = '0.7';
+
+		if (elem.style.pointerEvents)
+			elem.style.pointerEvents = 'none';
+	};
+
+	var after = function(elem) {
+		if (elem.style.opacity)
+			elem.style.opacity = '1';
+
+		if (elem.style.pointerEvents)
+			elem.style.pointerEvents = 'auto';
+	}
+
+})(jQuery);
 
 
-			 loadVariant(self.attr('data-variant'), before, after);
+document.getElementById('js-ps-ajax-wishlist-logged') && (function($) {
 
-			});
-
-		 var before = function(elem){
-		 	if(elem.style.opacity)
-		 		elem.style.opacity = '0.7';
-
-		 	if(elem.style.pointerEvents)
-		 		elem.style.pointerEvents = 'none';
-
-
-		 };
-
-		 var after = function(elem){
-		 	if(elem.style.opacity)
-		 		elem.style.opacity = '1';
-
-		 	if(elem.style.pointerEvents)
-		 		elem.style.pointerEvents = 'auto';
-
-		 }
-
-		})(jQuery);
-
-		document.getElementById('js-ps-ajax-wishlist-logged') && (function($){
-
-			$(document).on('click', '#js-ps-ajax-wishlist-logged', function(e){
-
-				e.preventDefault();
-
-				addMessage = function(type, message){
-
-					$('.alert') && $('.alert').remove();
-
-					$(document.body)
-					.prepend('<div class="row alert alert--'+type+'" id="js-ps-alert"><div class="container alert__container"><a class="alert__container__close" id="js-ps-alert-close" href="#" data-dismiss="alert">&times;</a><p class="alert__container__copy">'+message+'</p></div></div>');
-
-				};
-
-				$.ajax({
-					url: '/wishlist/rest/add.json',
-					data: {product_id: $('#js-ps-product-variant-id').val()},
-					type: 'GET',
-					cache: false,
-					success: function(data) {
-						!data.success && addMessage('error', 'There was a problem adding your product to the wishlis, please try again later.');
-						data.success === 0 && addMessage('error', data.msg); 
-						data.success === 1 && addMessage('success', data.msg);
-					}
-				}).fail(function(data){
-					addMessage('error', 'There was a problem adding your product to the wishlis, please try again later.');
-				});
-
-			})
-
-}(jQuery));
-document.getElementById('js-ps-ajax-wishlist') && (function($){
-
-	$(document).on('click', '#js-ps-ajax-wishlist', function(e){
+	$(document).on('click', '#js-ps-ajax-wishlist-logged', function(e) {
 
 		e.preventDefault();
 
-		window.location = '/wishlist/add/'+$('#js-ps-product-variant-id').val();
+		addMessage = function(type, message) {
+
+			$('.alert') && $('.alert').remove();
+
+			$(document.body)
+				.prepend('<div class="row alert alert--' + type + '" id="js-ps-alert"><div class="container alert__container"><a class="alert__container__close" id="js-ps-alert-close" href="#" data-dismiss="alert">&times;</a><p class="alert__container__copy">' + message + '</p></div></div>');
+
+		};
+
+		$.ajax({
+			url: '/wishlist/rest/add.json',
+			data: {
+				product_id: $('#js-ps-product-variant-id').val()
+			},
+			type: 'GET',
+			cache: false,
+			success: function(data) {
+				!data.success && addMessage('error', 'There was a problem adding your product to the wishlis, please try again later.');
+				data.success === 0 && addMessage('error', data.msg);
+				data.success === 1 && addMessage('success', data.msg);
+			}
+		}).fail(function(data) {
+			addMessage('error', 'There was a problem adding your product to the wishlis, please try again later.');
+		});
+
+	})
+
+}(jQuery));
+
+
+document.getElementById('js-ps-ajax-wishlist') && (function($) {
+
+	$(document).on('click', '#js-ps-ajax-wishlist', function(e) {
+
+		e.preventDefault();
+
+		window.location = '/wishlist/add/' + $('#js-ps-product-variant-id').val();
 
 
 	})
@@ -160,9 +157,9 @@ document.getElementById('js-ps-ajax-wishlist') && (function($){
 }(jQuery));
 
 
-document.getElementById('js-ps-share-email') && (function($){
+document.getElementById('js-ps-share-email') && (function($) {
 
-	$(document).on('click', '#js-ps-share-email', function(e){
+	$(document).on('click', '#js-ps-share-email', function(e) {
 		e.preventDefault();
 
 		!$('#js-ps-share-email-container').hasClass('is-open') && $('#js-ps-share-email-container').addClass('is-open');
@@ -173,41 +170,43 @@ document.getElementById('js-ps-share-email') && (function($){
 	var sent = false;
 
 
-	$(document).on('click', '#js-ps-share-email-container input[type="submit"]', function(e){
+	$(document).on('click', '#js-ps-share-email-container input[type="submit"]', function(e) {
 
 		e.preventDefault();
 
-		
+		var form = $('#js-ps-share-email-container form'),
+			formData = form.serialize(),
+			req = form.find('input[required="required"]'),
+			valid = true,
+			check = function(element) {
+				var evt = document.createEvent('KeyboardEvent');
+				evt.initKeyboardEvent('keyup');
+				document.createEvent ? element.get(0).dispatchEvent(evt) : element.get(0).fireEvent('onkeyup');
+			};
 
-		
-
-		var form = $('#js-ps-share-email-container form'), formData = form.serialize(), req = form.find('input[required="required"]'), valid = true, check = function(element){
-			var evt = document.createEvent('KeyboardEvent');
-			evt.initKeyboardEvent('keyup');
-			document.createEvent ? element.get(0).dispatchEvent(evt) : element.get(0).fireEvent('onkeyup');
-		};
-
-		req.each(function(){
+		req.each(function() {
 
 			var input = $(this);
 
 
-			if(input.val() === ''){
+			if (input.val() === '') {
 				check(input);
 				valid = false;
-			}	
+			}
 
 
 		});
 
 		if (sent === true)
 			return;
-	
 
-		if(valid === false || form.find('.help-inline').length > 0){ return; }
-	
+
+		if (valid === false || form.find('.help-inline').length > 0) {
+			return;
+		}
+
 		$('#js-ps-share-email-container').removeClass('is-open');
-		$('#js-ps-share-email-container form').get(0).reset(); 
+		$('#js-ps-share-email-container form').get(0).reset();
 
 
 		$.ajax({
@@ -227,18 +226,15 @@ document.getElementById('js-ps-share-email') && (function($){
 
 		sent = true;
 
-		
-
-
 	});
 
-$(document).on('click', '#js-ps-share-email-container-close', function(e){
+	$(document).on('click', '#js-ps-share-email-container-close', function(e) {
 
-	e.preventDefault();
+		e.preventDefault();
 
-	$('#js-ps-share-email-container').hasClass('is-open') && $('#js-ps-share-email-container').removeClass('is-open');
+		$('#js-ps-share-email-container').hasClass('is-open') && $('#js-ps-share-email-container').removeClass('is-open');
 
-});
+	});
 
 
 }(jQuery));
