5b636c4dfd2a06732f69c8549a1d0e9b789d5edf
cesar <cesar.castro@propcom.co.uk>
2014-10-28 11:21:47 +0000

    preventing countru select from firing at first change

--- js/modules/global/country.js
+++ js/modules/global/country.js
@@ -1,11 +1,14 @@
 ;document.getElementById('js-ps-country-select') && (function(){
 
 	var el = document.getElementById('js-ps-country-select'),
+		hasChanged = 0,
 		changeCountry = function(el){
 
 			el.options[el.selectedIndex] ? option = el.options[el.selectedIndex].value : option = '';
 
-			window.location = option;
+			hasChanged === 1 && window.location = option;
+
+			hasChanged = 1;
 
 		};
 
