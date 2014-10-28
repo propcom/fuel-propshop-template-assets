9fe7f55675692b624a464e4a48ced35b39be7aca
cesar <cesar.castro@propcom.co.uk>
2014-10-28 11:38:43 +0000

    improving on country script

--- js/modules/global/country.js
+++ js/modules/global/country.js
@@ -1,20 +1,16 @@
 ;document.getElementById('js-ps-country-select') && (function(){
 
 	var el = document.getElementById('js-ps-country-select'),
-		hasChanged = 0,
 		changeCountry = function(el){
 
 			el.options[el.selectedIndex] ? option = el.options[el.selectedIndex].value : option = '';
-
-			hasChanged === 1 && (function(){
 				window.location = option;
-			}());
-
-			hasChanged = 1;
 
 		};
 
-	el.addEventListener ? el.addEventListener('change', changeCountry(el)) : el.attachEvent('on' + 'change', function(){
+	!!el.addEventListener ? el.addEventListener('change', function(){
+		changeCountry(el);
+	}) : el.attachEvent('on' + 'change', function(){
       changeCountry.call(el);
     });
 
